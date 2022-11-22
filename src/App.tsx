5e5e8023eb9355a0caf8957ab7eaac556f942745
MatheusAndradeMelo <mandrademelo77@gmail.com>
2022-11-22 00:11:18 -0300

    feat: login screen with react, i decide to use typescript,and eslint for this simple code

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import stussy_img from './assets/stussy-1-logo-svg-vector.svg'
+import './App.css'
 
 function App() {
+
+    const [email, setEmail]= useState("")
+
+    const [password, setPassword]= useState("")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+          <form className="login-form">
+              <span className="login-form-title">
+                Bem-vindo!
+              </span>
+              <span className="login-form-title">
+                <img src={stussy_img} alt="" />
+              </span>
+
+              <div className="wrap-input">
+                <input 
+                className={email !== "" ? 'has-val input' : 'input'} 
+                type="email"
+                required
+                value={email}
+                onChange={ e => setEmail(e.target.value)}
+                />
+                <span className="focus-input" data-placeholder="Email"></span>
+              </div>
+
+              <div className="wrap-input">
+                <input 
+                className={password !== "" ? 'has-val input' : 'input'} 
+                type="password"
+                required
+                value={password}
+                onChange={ e => setPassword(e.target.value)}
+                />
+                <span className="focus-input" data-placeholder="Senha"></span>
+              </div>
+
+              <div className="container-login-form-btn">
+                <button className="login-form-btn">Login</button>
+              </div>
+
+              <div className="text-center">
+                <span className="text1">Não possui conta?</span>
+                <a href="#" className="text2">Criar conta</a>
+              </div>
+
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
